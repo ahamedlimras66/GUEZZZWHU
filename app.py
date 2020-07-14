0013ee7695848a9385536e8de58ef6a7e0441616
unknown <mm4.muthumani@gmail.com>
2020-07-14 15:54:29 +0530

    db add

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os
 from flask import Flask, render_template
+from models.schema import *
 
 app = Flask(__name__)
+app.secret_key = 'my-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route("/")
 def home():
@@ -36,4 +45,6 @@ def sample():
 
 
 if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
     app.run(debug=True)
---
+++ models/schema.py
@@ -0,0 +1,26 @@
+import datetime
+from db import db
+from flask_login import UserMixin
+from sqlalchemy import ForeignKey
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    gender = db.Column(db.String)
+    phone_no = db.Column(db.String(10))
+    email = db.Column(db.String(20))
+    dob = db.Column(db.DateTime, nullable=False)
+    user = db.relationship('Link', cascade="all,delete",backref="user")
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    link = db.relationship('LinkCommand', cascade="all,delete",backref="link")
+
+class LinkCommand(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    link_id = db.Column(db.Integer, ForeignKey('link.id'))
+    commander_id = db.Column(db.Integer, ForeignKey('user.id'))
+    command =  db.Column(db.String(255))
